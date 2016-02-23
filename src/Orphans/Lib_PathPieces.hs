92901f12993382634445f976519f226e835b4e27
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-02-23 15:49:49 +0100

    cleaning

---
+++ src/Orphans/Lib_PathPieces.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Orphans.Lib_PathPieces () where
+
+import qualified Data.Text      as T
+import           Web.PathPieces
+
+-- | Here we want to parse Double's from the URL.
+-- since parsing double is subjective to our need
+-- we have to define it.
+instance PathPiece Double where
+    fromPathPiece = Just . read . T.unpack
+    toPathPiece = T.pack . show
